b8dfdbd9ece1997da9e389384097a4dcc7c04983
jstable1 <jessica.stabler@gmail.com>
2022-05-10 09:46:01 -0600

    created react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+
+function App() {
+    return (
+        <div>
+            <Navigation
+    
+            ></Navigation>
+        </div>
+    )
+}
+
+export default App;
