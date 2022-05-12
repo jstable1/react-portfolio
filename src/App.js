b108ee54891653dd1b3fa7a627c4b4e580775022
jstable1 <jessica.stabler@gmail.com>
2022-05-11 21:50:08 -0600

    created footer and added icons and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
+import Footer from './components/Footer';
 // import Project from './components/Project';
 
 function App() {
     return (
         <div>
             <Header></Header>
+            <main>
+                
+            </main>
+            <Footer></Footer>
         </div>
     )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs';
+
+function Footer() {
+    return (
+        <footer id="contact-me" class="contact flex-row">
+            <div class="contact-options section-body">
+                <a href="https://github.com/jstable1"><BsGithub /></a>
+                <a href="https://www.linkedin.com/in/jessica-stabler-b6539012/"><BsLinkedin /></a>
+                <a href="https://twitter.com"><BsTwitter /></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
