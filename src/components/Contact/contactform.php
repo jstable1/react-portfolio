af39c61ac0d75cb4da3b6b31eb1fa994caacc527
jstable1 <jessica.stabler@gmail.com>
2022-09-12 13:07:59 -0600

    added subject line to contact form

--- src/components/Contact/contactform.php
+++ src/components/Contact/contactform.php
@@ -2,6 +2,7 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
+    $subject = $_POST['subject'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
@@ -9,6 +10,6 @@ if (isset($_POST['submit'])) {
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-    mail($mailTo, $txt, $headers);
+    mail($mailTo, $subject, $txt, $headers);
     header("Location: index.js?mailsend");
 }
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,13 +6,7 @@ function Contact() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
   const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-    }
-  };
+  const { name, subject, email, message } = formState;
 
   const handleChange = (e) => {
     if (e.target.name === 'email') {
@@ -37,34 +31,15 @@ function Contact() {
   return (
     <section class="contact">
       <h1 data-testid="h1tag">Contact Me</h1>
-      <form class="contact-form" onSubmit={handleSubmit} action="contactform.php" method="post">
+      <form class="contact-form" action="contactform.php" method="post">
         <div class="inputBoxes">
           <input type="text" name="name" defaultValue={name} onBlur={handleChange} placeholder="Name"></input>
+          <input type="text" name="subject" defaultValue={subject} onBlur={handleChange} placeholder="Subject"></input>
           <input type="text" name="email" defaultValue={email} onBlur={handleChange} placeholder="Your Email"></input>
           <textarea type="text" name="message" defaultValue={message} onBlur={handleChange} placeholder="Message"></textarea>
         </div>
         <button type="submit" class="contactButton" name="submit">Submit</button>
       </form>
-      {/* <form id="contact-form" onSubmit={handleSubmit}>
-        <div class="inputBoxes">
-          <label htmlFor="name">Name:</label><br></br>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div class="inputBoxes">
-          <label htmlFor="email">Email Address:</label><br></br>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div class="inputBoxes">
-          <label htmlFor="message">Message:</label><br></br>
-          <textarea name="message" rows="8" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit" class="contactButton">Submit</button>
-      </form> */}
       <h3>My email is jessica.stabler@gmail.com in case you prefer to email me later</h3>
     </section>
   );
