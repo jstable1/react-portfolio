9c33271caf7e943a437f012afdabc833996c609e
jstable1 <jessica.stabler@gmail.com>
2022-09-12 23:08:09 -0600

    updated contact component to remove form and updated css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import { MdOutlinePhoneIphone } from 'react-icons/md';
 
-
-
 function Contact() {
 
   return (
-    
-    <div class="contactFormOptions">
-      <a href="mailto:jessica.stabler@gmail.com"><AiOutlineMail size={35}/><span>jessica.stabler@gmail.com</span></a>
-      <p><MdOutlinePhoneIphone size={35}/><span>801-230-4660</span></p>
-    </div>
+    <section class="contact">
+      <h1>Contact Me:</h1>
+      <div class="contactFormOptions">
+        <a href="mailto:jessica.stabler@gmail.com"><AiOutlineMail size={35}/><span>jessica.stabler@gmail.com</span></a>
+        <p><MdOutlinePhoneIphone size={35}/><span>801-230-4660</span></p>
+      </div>
+    </section>
   );
 }
 
